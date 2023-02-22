473ef87b76f80653a9fc6e9d970abe2e4d3fe558
Nanashi <alksndr.zln@gmail.com>
2023-02-23 01:35:16 +0300

    Edited structure of reading data

--- quiz_base.py
+++ quiz_base.py
@@ -1,17 +1,28 @@
 import os
-import random
+import re
 
 
 def read_data():
+
     filename = os.environ['PATH_TXT']
-    with open(filename, 'r', encoding='utf-8') as file:
+    with open(filename, 'r', encoding='koi8-r') as file:
         data = file.readlines()
 
-    questions_base = {}
+    questions = {}
+    for i, j in enumerate(data):
+        if 'Вопрос' in data[i]:
+            index = i
+            question = data[index + 1]
+            while 'Комментарий' not in question:
+                index += 1
+                question += data[index + 1]
+            question = question.replace('\n', ' ')
+            question = question.split('Ответ:')
+            answer = question[1].split('.')[0]
+            question = question[0]
+            question = re.sub(r"[\(\[].*?[\)\]]", "", question).strip()
+            if 'Источник' in question or 'Дуплет' in question:
+                continue
+            questions[question] = answer.strip()
 
-    for pair in data:
-        question = pair.split('---')[0]
-        answer = pair.split('---')[1].replace('\n', '')
-        questions_base[question] = answer
-
-    return questions_base
+    return questions
